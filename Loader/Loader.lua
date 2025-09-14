47f6f76772a3f434f4b2038ac54469ed1efcf244
WataXScript <dalvo9216@gmail.com>
2025-09-14 18:55:48 +0800

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,4 +1,4 @@
-local url = "https://raw.githubusercontent.com/WataXScript/WataXFreeScript/main/Loader/main.lua"
+local url = "https://raw.githubusercontent.com/WataXScript/WataXFreeScript/main/Loader/fly.lua"
 local s, e = pcall(function()
     return loadstring(game:HttpGet(url))()
 end)
